13861cb82073a5adc38250ea189cfd5d11c196ab
jorg-j <47870433+jorg-j@users.noreply.github.com>
2020-10-11 11:49:02 +1100

    Update setup.sh

--- resources/location/setup.sh
+++ resources/location/setup.sh
@@ -5,3 +5,5 @@ sudo apt upgrade -y
 sudo apt install mcrypt
 
 wget https://raw.githubusercontent.com/jorg-j/jorg-j.github.io/master/resources/location/stat.txt
+
+crypt $N  < stat.txt > outfile.txt
