582b210a94a6d12e17a4fa0e8e2400df941679a6
Jack Jorgensen <jorgensen.jack@gmail.com>
2022-10-26 17:57:59 +1100

    tidy up

--- resources/ansible.sh
+++ resources/ansible.sh
@@ -9,7 +9,6 @@ PS3="Enter a number: "
 select filetype in RaspberryPi Yeti RPIScreen
 do
     echo "Selected: $filetype"
-    # echo "Selected number: $REPLY"
     break
 done
 
@@ -18,19 +17,19 @@ sudo apt update
 sudo apt install ansible git
 
 case $filetype in
-  RaspberryPi)
-    echo "Running Ansible Pull for Raspberry Pi"
-    ansible-pull -U http://192.168.1.122:8080/git/root/ansiblepull.git local-server-pi.yml
-  ;;
-  Yeti)
-    echo "Running Ansible Pull for Yeti"
-    ansible-pull -U http://192.168.1.122:8080/git/root/ansiblepull.git local-server-yeti.yml
-  ;;
-  RPIScreen)
-    echo "Running Ansible Pull for Raspberry Pi MQTT"
-    ansible-pull -U http://192.168.1.122:8080/git/root/ansiblepull.git main_7inch.yml
-  ;;
-  *)
-  printf "Invalid Selection\n"
-  ;;
+    RaspberryPi)
+        echo "Running Ansible Pull for Raspberry Pi"
+        ansible-pull -U http://192.168.1.122:8080/git/root/ansiblepull.git local-server-pi.yml
+    ;;
+    Yeti)
+        echo "Running Ansible Pull for Yeti"
+        ansible-pull -U http://192.168.1.122:8080/git/root/ansiblepull.git local-server-yeti.yml
+    ;;
+    RPIScreen)
+        echo "Running Ansible Pull for Raspberry Pi MQTT"
+        ansible-pull -U http://192.168.1.122:8080/git/root/ansiblepull.git main_7inch.yml
+    ;;
+    *)
+        printf "Invalid Selection\n"
+    ;;
 esac
